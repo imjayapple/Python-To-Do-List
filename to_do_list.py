ddb4487483982ac52bbf5d9283823b029ee0a978
imjayapple <imjayapple@gmail.com>
2023-03-27 22:24:27 -0700

    Initial commit, initial display menu configured

---
+++ to_do_list.py
@@ -0,0 +1,10 @@
+# Create a display menu
+
+def display_menu():
+    print("\nTo-Do List Menu")
+    print("1. View tasks")
+    print("2. Add a task")
+    print("3. Mark a task as completed")
+    print("4. Quit")
+
+    
