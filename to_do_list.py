99c6ef68d7336fc8e917a01280e1109a00b1044a
imjayapple <imjayapple@gmail.com>
2023-03-28 21:05:04 -0700

    View task feature

--- to_do_list.py
+++ to_do_list.py
@@ -7,4 +7,14 @@ def display_menu():
     print("3. Mark a task as completed")
     print("4. Quit")
 
+# Allow users to view their tasks
+
+def view_tasks(tasks):
+    if not tasks:
+        print("You have no tasks.")
+        return
     
+    print("\nYour tasks:")
+    for i, task in enumerate(tasks, start=1):
+        status= "completed" if task["completed"] else "Pending"
+        print(f"{i}. {task['description']} ({status})")
