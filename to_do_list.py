7a4233195a832f63b8e12240fc8d61b626987f28
imjayapple <imjayapple@gmail.com>
2023-03-31 23:22:52 -0700

    Main function for the to-do list application

--- to_do_list.py
+++ to_do_list.py
@@ -45,3 +45,32 @@ def mark_task_completed(tasks):
 
     tasks[task_number - 1]["completed"] = True
     print("Task marked as completed.")
+
+# Create the main loop in which the user will 'enter' the to-do list, receive user inputs & call appropriate functions
+
+def main():
+    tasks = []
+
+    while True:
+        display_menu()
+
+        try:
+            choice = int(input("Enter the number of the action you want to perform (1, 2, 3, or 4): "))
+            if choice not in range(1, 5):
+                raise ValueError
+        except ValueError:
+            print("Invalid input. Please enter an integer between 1 and 4.")
+            continue
+
+        if choice == 1:
+            view_tasks(tasks)
+        elif choice == 2:
+            add_task(tasks)
+        elif choice == 3:
+            mark_task_completed(tasks)
+        elif choice == 4:
+            print("Goodbye!")
+            break
+
+if __name__ == "__main__":
+    main()
